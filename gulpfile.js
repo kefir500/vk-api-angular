211c04d0de152e398f7b5b98ea4dad474d18e2ae
Alexander Gorishnyak <kefir500@gmail.com>
2017-05-29 16:06:06 +0600

    Reformat gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['build', 'watch']);
 
-gulp.task('scripts', function () {
+gulp.task('build', function () {
   return gulp.src('src/**/*.js')
-      .pipe(concat('vk-api-angular.js'))
-      .pipe(iife())
-      .pipe(gulp.dest('dist/'))
-      .pipe(uglify())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('dist/'));
+    .pipe(concat('vk-api-angular.js'))
+    .pipe(iife())
+    .pipe(gulp.dest('dist/'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('watch', ['default'], function () {
-  gulp.watch('src/**/*.js', ['scripts']);
+gulp.task('watch', function () {
+  gulp.watch('src/**/*.js', ['build']);
 });
